72cd7d11902095c3cedfe5d70f5fbdf0abef69a7
dearfad <dearfad@sina.com>
2022-05-06 12:05:18 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from patient_env import PatientEnv
-from stable_baselines3.common.env_checker import check_env
-
-
-env = PatientEnv(2)
-
-check_env(env)
+if __name__ == "__main__":
+    print('main')
